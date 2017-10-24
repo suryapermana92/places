8885ccc6919f41ba314fb94b683ba1f8debc29b5
Surya Permana <suryapermana@host-66-96-238-51.myrepublic.co.id>
2017-10-24 09:43:36 +0700

    commit

--- src/components/PlacesDetail.js
+++ src/components/PlacesDetail.js
@@ -5,17 +5,32 @@ import axios from 'axios';
 const PlacesDetail = (places) => {
     console.log(places.places)
     return (
-        <View style={styles.cardStyle}>
+        <View  style={styles.cardStyle}>
+        <View style={styles.topStyle}>
+            
+            </View>
+            <View style={styles.bottomStyle}>
             <Text style={styles.namaStyle}>{places.places.nama}</Text>
+                </View>
             </View>
     )
 
 }
 const styles = {
+    cardStyle: {
+    },
+    topStyle: {
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        flex: 5
+    },
     namaStyle: {
-        fontColor: 'orange',
-        fontSize: 20,
-        textAlign: 'bottom'
+        color: 'white',
+        fontSize: 20
+    },
+    bottomStyle: {
+        flex: 1,
+        backgroundColor: 'black'
     }
 }
 
